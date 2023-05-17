b42c2ee61ea2b010e4f4a537c735feabecd9fe42
David Spera <david.spera@libertymutual.com>
2023-05-17 11:13:14 -0400

    ch 6-1 exercises added check for term

--- myLabs/my_potus.py
+++ myLabs/my_potus.py
@@ -9,8 +9,7 @@ Created By: David Spera
 Created Date: 05/17/2023
 Modified Date:
 """
-# constants (AKA global variables -- keep these to a minimum)
-file_path = '../DATA/presidents.txt'
+
 # other functions
 def import_file(filename : str):
     """
@@ -39,22 +38,19 @@ def pres_record(term, name, bdate, ddate, bplace, bstate, tsdate, tedate, party)
     :return: dict
     """
     return {"term": int(term), "name": name, "dob": bdate, "died": ddate, "birthplace": f'{bplace}, {bstate}', "term_start": tsdate, "term_end": tedate, "party": party }
-def get_info(term : int)-> dict:
+def get_info(file : str, term : int)-> list:
     """
     Finds president record for provided term number
+    :param file: Path to file to import
     :param term: Number of term
     :return: List or String
     """
     try:
-        get_pres = import_file(file_path)
+        get_pres = import_file(file)
     except Exception as err:
         raise Exception(f'Error importing data from file: {err}') from err
     try:
         pres_info = [pres_record(*data) for data in get_pres if pres_record(*data)['term'] == term]
     except Exception as err:
         raise Exception(f'Error parsing info from provided term: {err}') from err
-    if pres_info:
-        return  pres_info
-    else:
-        return 'Term number does not exist'
-    return pres_info
+    return  pres_info
--- myLabs/my_potus_main.py
+++ myLabs/my_potus_main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-from potus import get_info
+from my_potus import get_info
 # other imports  (standard library, standard non-library, local)
 """
 Name: potus_main.py
@@ -9,24 +9,47 @@ Created By: David Spera
 Created Date: 05/17/2023
 Modified Date:
 """
+# constants (AKA global variables -- keep these to a minimum)
+file_path = '../DATA/presidents.txt'
+# other functions
+def import_file(filename : str):
+    """
+    Imports file and parses data
+    :type filename: object
+    :param filename: String of path to file
+    :return: None
+    """
+    # import data file
+    all_pres = []
+    with open(filename) as file:
+        m_term = []
+        for line in file:
+            (
+                term, lname, fname, bdate, ddate, bplace, bstate, tsdate, tedate,
+                party
+            ) = line[:-1].split(':')
+            m_term.append(int(term))
+    return max(m_term)
 # main function
 def main(args):
     """
-    This is the docstring for the main() function
-
+    Requests term number from user and returns President info
     :param args: Command line arguments.
     :return: None
     """
+    max_term = import_file(file_path)
     loop = True
     while loop:
         try:
             term = int(input('Enter in term number of president: '))
-            loop = False
+            if term > max_term or term == 0:
+                print('Term does not exist, try again')
+            else:
+                loop = False
         except ValueError as err:
             print('Must be a whole number, try again')
-    president = ''
     try:
-        president = get_info(term)
+        president = get_info(file_path, term)
     except Exception as err:
         print(err)
         return
